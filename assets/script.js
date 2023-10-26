8e92d663d324dd042c0e68de62a7a75ee5c9690a
CRNaro <144285637+CRNaro@users.noreply.github.com>
2023-10-25 20:08:48 -0400

    Build: Started work on question page and moving from main to Q page

--- assets/script.js
+++ assets/script.js
@@ -1,49 +1,98 @@
+//ToDo: set up variables for the quiz
 const timerElement = document.getElementById('timer');
 const startQuizButton = document.getElementById('start-quiz');
+const quizContainer = document.getElementById('quiz-container');
+const answerButtonsElement = document.getElementById('answer-buttons');
+const mainScreen = document.getElementById('main-screen');
+const questionScreen = document.getElementById('questionScreen');
 
 
+let questionElement = document.getElementById('question');
 
-//ToDo: set up variables for the quiz
-//ToDo: set up timer for the quiz - DONE (may need some tweaking)
-    let timeLeft = 60;
-    let timerInterval;
-    
-    startQuizButton.addEventListener('click', startQuiz);
+// ToDo: set up function to display the questions
+// ToDo: set up prompts for questions to be used for the quiz (NOTE: NEED TO REARRANGE ANSWERS)
+const questions = [{
+      questionText: 'What element do we use in HTML to connect our JavaScript file?',
+      answers: [
+        { text: '<script>', correct: true },
+        { text: '<link>', correct: false },
+        { text: '<meta>', correct: false },
+        { text: '<style>', correct: false },
+      ]
+}, {     
+      questionText: 'What is declaration is used for a variable that can NOT be changed?',
+      answers: [
+        { text: 'const', correct: true },
+        { text: 'var', correct: false },
+        { text: 'let', correct: false },
+        { text: 'none of the above', correct: false },
+      ]
+}, {  
+      questionText: 'What is the correct syntax for an array?',
+      answers: [
+        { text: 'var array = []', correct: true },
+        { text: 'var array = {}', correct: false },
+        { text: 'var array = ()', correct: false },
+        { text: 'var array = <>', correct: false },
+      ]
+}, {
+        questionText: 'What is the correct syntax for a function?',
+        answers: [
+          { text: 'function = myFunction()', correct: true },
+          { text: 'function myFunction()', correct: false },
+          { text: 'function: myFunction()', correct: false },
+          { text: 'function myFunction[]', correct: false },
+        ] 
+}, {
+        questionText: 'How can we prevent default behavior of a link or form in JavaScript?',
+        answers: [
+          { text: 'preventDefault()', correct: true },
+          { text: 'preventDefault', correct: false },
+          { text: 'preventDefault = true', correct: false },
+          { text: 'preventDefault = false', correct: false },
+        ]
+}];
 
-    function startQuiz() {
-      timerInterval = setInterval(updateTimer, 1000);
+
+let timeLeft = 60;
+let timerInterval;
+
+startQuizButton.addEventListener('click', startQuiz);
+
+function startQuiz() {
+  timerInterval = setInterval(updateTimer, 1000);
 }
-    
-    // Timer starter
-    function updateTimer() {
-      const minutes = Math.floor(timeLeft / 60);
-      let seconds = timeLeft % 60;
 
-      seconds = seconds < 10 ? '0' + seconds : seconds;
+// Timer starter
+function updateTimer() {
+  const minutes = Math.floor(timeLeft / 60);
+  let seconds = timeLeft % 60;
 
-      timerElement.innerHTML = `${minutes}:${seconds}`;
-      timeLeft--;
+  seconds = seconds < 10 ? '0' + seconds : seconds;
 
-      if (timeLeft < 0) {
-        clearInterval(timerInterval);
-        timerElement.textContent('Time is up!');
-      }
-    }
-    function startQuiz() {
-      timerInterval = setInterval(updateTimer, 1000);
-      
-      quizQuestion1();
-   }
+  timerElement.innerHTML = `${minutes}:${seconds}`;
+  timeLeft--;
 
-    document.addEventListener('DOMContentLoaded', (event) => {
-    startQuizButton.addEventListener('click', startQuiz);
+  if (timeLeft < 0) {
+    clearInterval(timerInterval);
+    timerElement.textContent('Time is up!');
+  }
+}
+function startQuiz() {
+  timerInterval = setInterval(updateTimer, 1000);
+  
+  quizQuestion1();
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+startQuizButton.addEventListener('click', startQuiz);
 });
 
 //ToDo: set up function to start the quiz
 //ToDo: set up function to end the quiz
 //ToDo: set up function to save the score
 //ToDo: set up function to display the score
-//ToDo: set up function to display the questions
+
 //ToDo: set up function to check the answers
 //ToDo: set up function to display the high scores
 //ToDo: set up function to clear the high scores
@@ -52,5 +101,5 @@ const startQuizButton = document.getElementById('start-quiz');
 
 
 
-// ToDo: set up prompts for questions to be used for the quiz
+
 
--- assets/script2.js
+++ assets/script2.js
@@ -70,3 +70,106 @@ for (let i = 0; i < choices.length; i++) {
     document.body.appendChild(questionElement);
 
     // display the choices as buttons
+
+
+    function updateTimer() {
+        const minutes = Math.floor(timeLeft / 60);
+        let seconds = timeLeft % 60;
+  
+        seconds = seconds < 10 ? '0' + seconds : seconds;
+  
+        timerElement.innerHTML = `${minutes}:${seconds}`;
+        timeLeft--;
+  
+        if (timeLeft < 0) {
+          clearInterval(timerInterval);
+          timerElement.textContent('Time is up!');
+        }
+      }
+      function startQuiz() {
+        timerInterval = setInterval(updateTimer, 1000);
+        
+        quizQuestion1();
+     }
+  
+      document.addEventListener('DOMContentLoaded', (event) => {
+      startQuizButton.addEventListener('click', startQuiz);
+  });
+  
+  let currentQuestionIndex = 0;
+
+  startQuizButton.addEventListener('click', startQuiz); 
+  
+  function startQuiz() {
+    startQuizButton.classList.add('hide');
+    quizContainer.classList.remove('hide');
+    setNextQuestion();
+  }
+  
+  function showQuestion(question) {
+    questionElement.innerText = question.questionText;
+    answerButtonsElement.innerHTML = '';
+    question.answers.forEach(answer => {
+      const button = document.createElement('button');
+      button.innerText = answer.text;
+      button.classList.add('btn');
+      if (answer.correct) {
+        button.dataset.correct = answer.correct;
+      }
+      button.addEventListener('click', selectAnswer);
+      answerButtonsElement.appendChild(button);
+    });
+  }
+  
+  function selectAnswer() {
+    const selectedButton = this;
+    const correct = selectedButton.dataset.correct;
+    setStatusClass(document.body, correct);
+    Array.from(answerButtonsElement.children).forEach(button => {
+      setStatusClass(button, button.dataset.correct);
+    });
+    if (currentQuestionIndex < questions.length - 1) {
+      currentQuestionIndex++;
+      showQuestion(questions[currentQuestionIndex]);
+  } else {
+    endQuiz();
+  }
+  }
+  
+  function setStatusClass(element, correct) {
+    clearStatusClass(element);
+    if (correct) {
+      element.classList.add('correct');
+    } else {
+      element.classList.add('wrong');
+    }
+  }
+  
+  function clearStatusClass(element) {
+    element.classList.remove('correct');
+    element.classList.remove('wrong');
+  }
+  
+  
+  function startTimer() {
+    let timeLeft = 60;
+    const timerElement = document.getElementById('timer');
+    const timerInterval = setInterval(() => {
+      const minutes = Math.floor(timeLeft / 60);
+      let seconds = timeLeft % 60;
+      seconds = seconds < 10 ? '0' + seconds : seconds;
+      timerElement.textContent = `${minutes}:${seconds}`;
+      timeLeft--;
+      if (timeLeft < 0) {
+        clearInterval(timerInterval);
+        timerElement.textContent = 'Time is up!';
+        endQuiz();
+      }
+    }, 1000);
+  }
+  
+  function endQuiz() {
+    quizContainer.classList.add('hide');
+    startQuizButton.classList.remove('hide');
+    currentQuestionIndex = 0;
+  }
