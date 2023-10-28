32d0761cbaf2fabca670adcaafbc4cd7de2388ec
CRNaro <144285637+CRNaro@users.noreply.github.com>
2023-10-28 10:38:18 -0400

    Build/Fix: Started over, to clean up code, make more readable and create a better flow through functions.  seems to be better so far.'

--- assets/script.js
+++ assets/script.js
@@ -1,128 +1,131 @@
-//ToDo: set up variables for the quiz
-const highScroresBtn = document.getElementById('see-highest-score'); //connected
-const timerElement = document.getElementById('timer'); //connected
-const mainScreen = document.getElementById('main-screen'); //connected
-const startQuizButton = document.getElementById('start-quiz'); //connected
-const questionScreen = document.getElementById('question-screen'); //connected
-const answerButtonsElement = document.getElementById('answer-buttons');//connected
-const endScreen = document.getElementById('end-window'); //connected
-const finalScore = document.getElementById('final-score'); //connected
-const scoreForm = document.getElementById('score'); //connected
-const initials = document.getElementById('initials'); //connected
-const submitScoreBtn = document.getElementById('submit-score'); //connected
+//Timer elements
+const timerEl = document.querySelector('#timer');
+const minutesEl = document.querySelector('#minutes');
+const secondsEl = document.querySelector('#seconds');
+const startScreen = document.querySelector('#start-screen');
+const startBtn = document.querySelector('#start-button');
+const endScreen = document.querySelector('#end-screen');
+const questionText= document.querySelector('#question-text');
+const quizScreen = document.querySelector('#quiz-screen'); //!!!! USE qE or quizScreen?
+const answerButtons = document.querySelector('#answer-buttons');
 
-
-let questionElement = document.getElementById('question'); 
-
-// ToDo: set up function to display the questions - 
-//       switch from start screen to question screen
-// ToDo: set up prompts for questions to be used for the quiz (NOTE: NEED TO REARRANGE ANSWERS)
-let questions = [{
-      questionText: 'What element do we use in HTML to connect our JavaScript file?',
-      answers: [
-          '<script>', 
-          '<link>', 
-          '<meta>',
-          '<style>',
-      ],
-      answer: '<script>',
-}, {     
-      questionText: 'What is declaration is used for a variable that can NOT be changed?',
-      answers: [
-          'const',
-          'var',
-          'let',
-          'none of the above',
-      ],
-      answer: 'const',
-}, {  
-      questionText: 'What is the correct syntax for an array?',
-      answers: [
-          'var array = []',
-          'var array = {}', 
-          'var array = ()', 
-          'var array = <>', 
-      ],
-      answer: 'var array = []',
-}, {
-        questionText: 'What is the correct syntax for a function?',
-        answer: [
-            'function = myFunction()', 
-            'function myFunction()', 
-            'function: myFunction()', 
-            'function myFunction[]', 
-        ], 
-        answer: 'function myFunction()',
-}, {
-        questionText: 'How can we prevent default behavior of a link or form in JavaScript?',
-        answers: [
-            'preventDefault()', 
-            'preventDefault', 
-            'preventDefault = true', 
-            'preventDefault = false', 
-        ],
-        answer: 'preventDefault()',
-}];
-
-
-let timeLeft = 60;
+let currentIndex = 0;
+let score = 0;
 let timerInterval;
-  // Start Quiz Button
-startQuizButton.addEventListener('click', startQuiz);
-  // Hide main screen  
-  function startQuiz() {
-    document.mainScreen.style.display = 'none';
-      // Show question screen
-    document.questionScreen.style.display = 'block';
-      // Display question 1
-      displayQuestionText(0);
+
+function startQuiz() {
+    startScreen.style.display = 'none';
+    quizScreen.style.display = 'block';
+    displayQuestion(currentIndex);
+    startTimer();
+}
+
+function displayQuestion(index) {
+    let question = questions[index];
+    questionText.innerText = question.questionText;
+    for (let i = 0; i < question.answers.length; i++) {
+        let button = document.createElement('button');
+        button.innerText = question.answers[i];
+        button.addEventListener('click', function () {
+            checkAnswer(question.answers[i], question.answer);
+            currentIndex++;
+            if (currentIndex < questions.length) {
+                displayQuestion(currentIndex);
+            } else {
+                endQuiz();
+            }
+        });
+        answerButtons.appendChild(button);
     }
-
-function displayQuestion(questionIndex) {
-  const questions = questions[questionIndex].questionText;
-  const answerChoices = questions[questionIndex].answerChoices;
 }
 
-
-
-
-function startQuiz() {
-  timerInterval = setInterval(updateTimer, 1000);
+function checkAnswer(answer, correctAnswer) {
+    if (answer === correctAnswer) {
+        score++;
+    }
 }
 
-// Timer starter
-function updateTimer() {
-  const minutes = Math.floor(timeLeft / 60);
-  let seconds = timeLeft % 60;
+function startTimer() {
+    let time = 60;
+    timerInterval = setInterval(function () {
+        time--;
+        let minutes = Math.floor(time / 60);
+        let seconds = time % 60;
+        timerEl.textContent = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
 
-  seconds = seconds < 10 ? '0' + seconds : seconds;
+        if (time === 0) {
+            clearInterval(timerInterval);
+            endQuiz();
+        }
+    }, 1000);
+}
 
-  timerElement.innerHTML = `${minutes}:${seconds}`;
-  timeLeft--;
-
-  if (timeLeft < 0) {
+function endQuiz() {
     clearInterval(timerInterval);
-    timerElement.textContent('Time is up!');
-  }
-}
-function startQuiz() {
-  timerInterval = setInterval(updateTimer, 1000);
-  
-  
+    quizScreen.style.display = 'none';
+    endScreen.style.display = 'block';
+    document.querySelector('#final-score').innerText = `Your final score is: ${score}`;
 }
 
-document.addEventListener('DOMContentLoaded', (event) => {
+
+startBtn.addEventListener('click', () => {
+    // Start timer
+    startTimer();
+   // Hide start screen
+   startQuiz();
+   startScreen.style.display = 'none';
+   // Show question screen
+   quizScreen(); // ! saying property is undefined (reading 'display')
 
 });
 
-// To Do: Wrong answer buttons need to be tied to 2-5 second penalty
-// To Do: All answers need to be tied to next question screen
-// To Do: When quiz is finished timer needs to stop and that is the score
-// To Do: Need to set up local storage to save high scores/initials -
-//        probably need to set up a second HTML page for this
-// To Do: ONCE ALL FUNCTIONS IN JS ARE WORKING, NEED TO CLEAN UP CSS!!!
 
 
 
 
-
+let questions = [{
+    questionText: 'What element do we use in HTML to connect our JavaScript file?',
+    answers: [
+        '<script>', 
+        '<link>', 
+        '<meta>',
+        '<style>',
+    ],
+    answer: '<script>',
+}, {     
+    questionText: 'What is declaration is used for a variable that can NOT be changed?',
+    answers: [
+        'const',
+        'var',
+        'let',
+        'none of the above',
+    ],
+    answer: 'const',
+}, {  
+    questionText: 'What is the correct syntax for an array?',
+    answers: [
+        'var array = []',
+        'var array = {}', 
+        'var array = ()', 
+        'var array = <>', 
+    ],
+    answer: 'var array = []',
+}, {
+      questionText: 'What is the correct syntax for a function?',
+      answer: [
+          'function = myFunction()', 
+          'function myFunction()', 
+          'function: myFunction()', 
+          'function myFunction[]', 
+      ], 
+      answer: 'function myFunction()',
+}, {
+      questionText: 'How can we prevent default behavior of a link or form in JavaScript?',
+      answers: [
+          'preventDefault()', 
+          'preventDefault', 
+          'preventDefault = true', 
+          'preventDefault = false', 
+      ],
+      answer: 'preventDefault()',
+}];
